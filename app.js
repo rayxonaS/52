1f9cf14cd21449ca0249f0c9c105d345295b1161
rayxona <rayxona@gmail.com>
2025-02-22 13:22:34 +0500

    Add initial HTML structure and JavaScript for DOM manipulation

---
+++ app.js
@@ -0,0 +1,13 @@
+/*const article = document.querySelector("article");
+console.log(article.children);
+Array.from(article.children).forEach((child) => {
+  child.classList.add("article-child");
+});*/
+
+const title = document.querySelector("h2");
+console.log(title.parentElement);
+console.log(title.parentElement.parentElement);
+console.log(title.nextElementSibling);
+console.log(title.previousElementSibling);
+
+console.log(title.nextElementSibling.parentElement.children);
