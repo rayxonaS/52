89bb30bbaaa022ac5a874dc237535b30d666e1eb
rayxona <rayxona@gmail.com>
2025-02-24 13:43:07 +0500

    Enhance modal functionality: add event listener for overlay click and Escape key to close modal

--- app.js
+++ app.js
@@ -3,12 +3,25 @@ const modal = document.getElementById("modal");
 const closeBtn = document.getElementById("close-btn");
 const overlay = document.getElementById("overlay");
 
+const addHidden = () => {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
 showBtn.addEventListener("click", () => {
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
 });
 
 closeBtn.addEventListener("click", () => {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
+  addHidden();
+});
+
+overlay.addEventListener("click", () => {
+  addHidden();
+});
+
+document.addEventListener("keydown", (e) => {
+  if (e.key == "Escape") {
+    addHidden();
+  }
 });
