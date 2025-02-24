265ba3bab6b5ebd4610626bd221befc0df14d773
rayxona <rayxona@gmail.com>
2025-02-24 13:46:19 +0500

    Refactor modal event listeners: extract removeHidden function and streamline close actions

--- app.js
+++ app.js
@@ -7,18 +7,16 @@ const addHidden = () => {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
 };
-showBtn.addEventListener("click", () => {
+
+const removeHidden = () => {
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
-});
+};
+showBtn.addEventListener("click", removeHidden);
 
-closeBtn.addEventListener("click", () => {
-  addHidden();
-});
+closeBtn.addEventListener("click", addHidden);
 
-overlay.addEventListener("click", () => {
-  addHidden();
-});
+overlay.addEventListener("click", addHidden);
 
 document.addEventListener("keydown", (e) => {
   if (e.key == "Escape") {
